56e72232b4376e0ee131b2f6b444ec4e8c99bce6
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 09:08:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(arr=[" "," "," "," "," "," "," "," "," "],index=1)
-  if arr[index]==" "
-    return false
-  elsif arr[index]==nil
+  if (arr[index]==" ")||(arr[index]=nil)||(arr[index]="")
     return false
   end
   return true
